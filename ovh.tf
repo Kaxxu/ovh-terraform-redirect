2d51523a35d678e885dbf0799af76dc5d8a2260e
Kaxxu <pawmentel@gmail.com>
2023-03-05 16:06:50 +0100

    fix: redirection fix

--- ovh.tf
+++ ovh.tf
@@ -1,8 +1,8 @@
 resource "ovh_domain_zone_redirection" "main" {
   for_each = var.ovh_redirect
 
-  subdomain = "${each.key}.${var.domain}"
+  subdomain = each.key
+  zone      = var.domain
   target    = each.value
   type      = var.ovh_type
-  zone      = var.domain
 }
